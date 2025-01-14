a903df12a19c8b5ccb312c0f308aecd8e43cd560
Artem Lytvynenko <artem.lytvynenko@codility.com>
2025-01-14 13:34:25 +0100

    add mongo client to fastapi lifespan

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,25 @@
-from fastapi import FastAPI
+import logging
 
-app = FastAPI()
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from backend import settings
+from backend.routers.stories import router as stories_router
+
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.mongodb_client = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.mongodb = app.mongodb_client[settings.MONGODB_DB_NAME]
+    logger.info(f"Connected to MongoDB: {app.mongodb}")
+
+    yield
+
+    app.mongodb_client.close()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(stories_router)
