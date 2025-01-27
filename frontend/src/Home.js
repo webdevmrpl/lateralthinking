553b879f016c27c59dbe2663489dff4cc318cf47
belovezhalin <angelina.belovezha@gmail.com>
2025-01-27 20:07:20 +0000

    refactored frontend file structure

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Cookies from 'js-cookie';
-import './Home.css';
+import './styles/Home.css';
 
 function Home() {
     const [stories, setStories] = useState([]);
