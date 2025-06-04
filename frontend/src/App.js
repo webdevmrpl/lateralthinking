8d836af406bcffd441d378f13dbf4ff1138c6ea1
belovezhalin <angelina.belovezha@gmail.com>
2025-06-04 11:40:24 +0200

    updated main App component to include new auth system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/auth/PtotectedRoute';
+import LoginForm from './components/auth/LoginForm';
+import RegisterForm from './components/auth/RegisterForm';
 import Home from './Home';
 import Game from './Game';
-
+import Leaderboard from './Leaderboard';
 
 function App() {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/game/:storyTitle/:storyId" element={<Game />} />
-            </Routes>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/register" element={<RegisterForm />} />
+                    <Route
+                        path="/"
+                        element={
+                            <ProtectedRoute>
+                                <Home />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="/game/:storyId"
+                        element={
+                            <ProtectedRoute>
+                                <Game />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="/leaderboard"
+                        element={
+                            <ProtectedRoute>
+                                <Leaderboard />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+            </AuthProvider>
         </Router>
     );
 }
